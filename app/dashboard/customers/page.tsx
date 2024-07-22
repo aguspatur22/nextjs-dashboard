1e5ba04c4bf8fd44401fece2f7e0fe02ec5838c5
Agustin Paturlanne <aguspaturlanne@gmail.com>
2024-07-22 12:45:29 -0300

    Customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,32 @@
+import Table from '@/app/ui/customers/table';
 import { Metadata } from 'next';
- 
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
+
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Table query={query}/>
+    </div>
+  );
+}
